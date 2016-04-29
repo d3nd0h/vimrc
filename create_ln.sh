ad2b013b52a38b26f2c32dca387e9446bfe962e1
Sendy Halim <sendyhalim93@gmail.com>
2016-04-29 10:46:37 +0700

    Add neovimrc

--- create_ln.sh
+++ create_ln.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-ln -s $(pwd)/vimrc ~/.vimrc
+NEOVIM_DIR=$HOME/.config/nvim
+
+if [ ! -d $NEOVIM_DIR ]; then
+    mkdir -p $NEOVIM_DIR
+fi
+
+ln -s $(pwd)/vimrc $NEOVIM_DIR/init.vim
+ln -s $(pwd)/vimrc ~/.neovimrc
 ln -s $(pwd)/ideavimrc ~/.ideavimrc
 ln -s $(pwd)/xvimrc ~/.xvimrc
 ln -s $(pwd)/UltiSnips ~/.vim/UltiSnips
