cb4debf69c946b88ba94428c55826cb89fb2cafd
Sendy Halim <sendyhalim93@gmail.com>
2016-07-01 14:20:57 +0700

    Link to vimrc

--- create_ln.sh
+++ create_ln.sh
@@ -6,6 +6,7 @@ VIM_DIR=$HOME/.vim
 ln -s $VIM_DIR $NEOVIM_DIR
 ln -s $(pwd)/vimrc $NEOVIM_DIR/init.vim
 ln -s $(pwd)/vimrc ~/.neovimrc
+ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/ideavimrc ~/.ideavimrc
 ln -s $(pwd)/xvimrc ~/.xvimrc
 ln -s $(pwd)/UltiSnips ~/.vim/UltiSnips
